47b20157e154f9296e467e30eb91c7aefcdf8848
Nils van Schaik <nils.van.schaik@inspark.nl>
2023-08-21 16:37:36 +0200

    Add to UI

---
+++ Kerbee/Functions/RemoveKey.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+
+using Kerbee.Graph;
+
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Kerbee.Functions
+{
+    public class RemoveKey
+    {
+        private readonly ILogger _logger;
+        private readonly IApplicationService _applicationService;
+
+        public RemoveKey(
+            IApplicationService applicationService,
+            ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<RemoveKey>();
+            _applicationService = applicationService;
+        }
+
+        [Function($"{nameof(RemoveKey)}_{nameof(HttpStart)}")]
+        public async Task<HttpResponseData> HttpStart([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            await _applicationService.RemoveKeyAsync(applicationId);
+            var response = req.CreateResponse(HttpStatusCode.OK);
+
+            return response;
+        }
+    }
+}
