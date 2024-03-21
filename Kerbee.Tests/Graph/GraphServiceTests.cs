744e618d05a820c25dcfefcfd234e44047b14615
Maarten <17746901+snerte@users.noreply.github.com>
2024-03-21 15:46:34 +0100

    Adding other tests

--- Kerbee.Tests/Graph/GraphServiceTests.cs
+++ Kerbee.Tests/Graph/GraphServiceTests.cs
@@ -12,22 +12,53 @@ namespace Kerbee.Tests.Graph;
 
 public class GraphServiceTests
 {
+    private static readonly AutoMocker _mocker = new();
+    private static readonly Mock<IRequestAdapter> _requestAdapter = new();
+
+    private static GraphService Sut => _mocker.CreateInstance<GraphService>();
+
+
     [Theory, AutoData]
     public async Task GetApplicationAsync_ShouldUseManagedIdentityClient(string objectId)
     {
         // Arrange
-        var mocker = new AutoMocker();
-
-        var requestAdapter = mocker.Get<Mock<IRequestAdapter>>();
-
-        mocker.Use<IManagedIdentityProvider>(x => x.GetClient() == new GraphServiceClient(requestAdapter.Object, null));
+        SetupManagedIdentityProvider();
 
         // Act
-        var sut = mocker.CreateInstance<GraphService>();
-
-        await sut.GetApplicationAsync(objectId);
+        await Sut.GetApplicationAsync(objectId);
 
         // Assert
-        mocker.VerifyAll();
+        _mocker.VerifyAll();
+    }
+
+    [Theory, AutoData]
+    public async Task RemoveCertificateAsync_ShouldUseManagedIdentityClient(string objectId, string keyId)
+    {
+        // Arrange
+        SetupManagedIdentityProvider();
+
+        // Act
+        await Sut.RemoveCertificateAsync(objectId, keyId);
+
+        // Assert
+        _mocker.VerifyAll();
+    }
+
+    [Theory, AutoData]
+    public async Task RemoveSecretAsync_ShouldUseManagedIdentityClient(string objectId)
+    {
+        // Arrange
+        SetupManagedIdentityProvider();
+
+        // Act
+        await Sut.GetApplicationAsync(objectId);
+
+        // Assert
+        _mocker.VerifyAll();
+    }
+
+    private static void SetupManagedIdentityProvider()
+    {
+        _mocker.Use<IManagedIdentityProvider>(x => x.GetClient() == new GraphServiceClient(_requestAdapter.Object, null));
     }
 }
