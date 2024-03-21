0b6544a7a34b5559d4641d4339430f7348307d7c
Maarten <17746901+snerte@users.noreply.github.com>
2024-03-21 15:39:30 +0100

    Adding first test

---
+++ Kerbee.Tests/Graph/GraphServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using AutoFixture.Xunit2;
+
+using Kerbee.Graph;
+
+using Microsoft.Graph;
+using Microsoft.Kiota.Abstractions;
+
+using Moq;
+using Moq.AutoMock;
+
+namespace Kerbee.Tests.Graph;
+
+public class GraphServiceTests
+{
+    [Theory, AutoData]
+    public async Task GetApplicationAsync_ShouldUseManagedIdentityClient(string objectId)
+    {
+        // Arrange
+        var mocker = new AutoMocker();
+
+        var requestAdapter = mocker.Get<Mock<IRequestAdapter>>();
+
+        mocker.Use<IManagedIdentityProvider>(x => x.GetClient() == new GraphServiceClient(requestAdapter.Object, null));
+
+        // Act
+        var sut = mocker.CreateInstance<GraphService>();
+
+        await sut.GetApplicationAsync(objectId);
+
+        // Assert
+        mocker.VerifyAll();
+    }
+}
