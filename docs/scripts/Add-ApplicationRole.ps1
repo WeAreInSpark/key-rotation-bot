e7603d122d16f6e741ed585f1b805a43f88b0672
Maarten <17746901+snerte@users.noreply.github.com>
2025-01-15 11:58:52 +0100

    Finishing rewrite

--- docs/scripts/Add-ApplicationRole.ps1
+++ docs/scripts/Add-ApplicationRole.ps1
@@ -14,12 +14,10 @@ az login --tenant $TenantId
 $kerbeePrincipalId = az ad sp show --id $AppId --query "id" --output tsv
 
 # Get the Microsoft Graph service principal
-$msGraphServicePrincipalId = az ad sp list --filter "appId eq '00000003-0000-0000-c000-000000000000'" --query "[0].objectId" -o tsv
+$msGraphServicePrincipalId = az ad sp show --id '00000003-0000-0000-c000-000000000000' --query "id" --output tsv
 
 # Get the app role ID for "Application.ReadWrite.OwnedBy"
 $appRoleId = az ad sp show --id $msGraphServicePrincipalId --query "appRoles[?value=='Application.ReadWrite.OwnedBy' && contains(allowedMemberTypes, 'Application')].id" -o tsv
 
 # Assign the app role to the service principal
 az ad sp create --id $kerbeePrincipalId --role $appRoleId --scope $msGraphServicePrincipalId
-
-Write-Output "App role assigned successfully."
